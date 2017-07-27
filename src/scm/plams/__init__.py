2520fca9b4ab3e2e8875921e31830aa4b55f6167
Michal Handzlik <handzlik@scm.com>
2017-07-27 16:38:13 +0200

    Automatic import system

--- src/scm/plams/__init__.py
+++ src/scm/plams/__init__.py
@@ -1,22 +1,32 @@
-from .core.basejob import *
-from .core.basemol import *
-from .core.common import *
-from .core.errors import *
-from .core.jobrunner import *
-from .core.jobmanager import *
-from .core.results import *
-from .core.settings import *
+"""
+What follows is a somewhat hacky way to set up an automatic plug'n'play import mechanism. If you experience any problems with importing, remove this and bring back good old explicit imports:
 
-from .tools.geometry import *
-from .tools.kftools import *
-from .tools.pdbtools import *
-from .tools.periodic_table import *
-from .tools.numdiff import *
-from .tools.units import *
+        from .core.basejob import *
+        from .core.basemol import *
+        ...
+        from .tools.geometry import *
+        from .tools.kftools import *
+        ...
+        from .interfaces.adfsuite import *
+        from .interfaces.cp2k import *
+"""
 
-from .interfaces.adfsuite import *
-from .interfaces.cp2k import *
-from .interfaces.dirac import *
-from .interfaces.gamess import *
-from .interfaces.mopac import *
-from .interfaces.orca import *
+def __import_all(path):
+    """Traverse the directory tree rooted in *path* and find all Python modules living there. For each module, import everything given by its __all__ variable to the globals() namespace."""
+    import os
+    is_module = lambda x: x.endswith('.py') and not x.startswith('__init__')
+
+    ret = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        modules = [os.path.splitext(f)[0] for f in filter(is_module, filenames)]
+        relpath = dirpath.replace(path+'/', '').split('/')
+        for mod in modules:
+            imp = '.'.join(relpath + [mod])
+            tmp = __import__(imp, globals=globals(), fromlist=['*'], level=1)
+            if hasattr(tmp, '__all__'):
+                ret += tmp.__all__
+                for name in tmp.__all__:
+                    globals()[name] = vars(tmp)[name]
+    return ret
+
+__all__ = __import_all(__path__[0])
