ef25cf863fb20ae8a3ba4a2649e7643f85097770
Michal Handzlik <handzlik@scm.com>
2017-08-08 17:21:28 +0200

    UFF interface added

--- src/scm/plams/interfaces/adfsuite/uff.py
+++ src/scm/plams/interfaces/adfsuite/uff.py
@@ -1,5 +1,10 @@
 from .scmjob import SCMJob, SCMResults
 
+
+__all__ = ['UFFJob', 'UFFResults']
+
+
+
 class UFFResults(SCMResults):
     _kfext = '.rkf'
     _rename_map = {'uff.rkf':'$JN'+_kfext}
@@ -19,17 +24,30 @@ class UFFJob(SCMJob):
         printtypes = all(map(lambda at: ('uff' in at.properties and 'type' in at.properties.uff), self.molecule))
 
         for i,atom in enumerate(self.molecule):
-            s[system]['atoms']['_'+str(i+1)] = atom.str(symbol=self._atom_symbol(atom), space=18, decimal=10)
+            suffix = ''
+            suffix_dict = {}
+            if printtypes:
+                suffix = '  {type}  {charge}'
+                suffix_dict = atom.properties.uff.copy()
+                if 'charge' not in suffix_dict:
+                    suffix_dict.charge = 0.0
+            s[system]['atoms']['_'+str(i+1)] = atom.str(suffix=suffix, suffix_dict=suffix_dict, space=18, decimal=10)
+
+        if 'ignore_bonds' not in self.settings:
+            self.molecule.set_atoms_id()
+            for i,bond in enumerate(self.molecule.bonds):
+                s[system]['bonds']['_'+str(i+1)] = '{:6d}  {:6d}  {:6.2f}'.format(bond.atom1.id, bond.atom2.id, bond.order)
+            self.molecule.unset_atoms_id()
+
         if self.molecule.lattice:
             for i,vec in enumerate(self.molecule.lattice):
-                s[s.find_case('system')]['lattice']['_'+str(i+1)] = '%16.10f %16.10f %16.10f'%vec
+                s[system]['lattice']['_'+str(i+1)] = '{:16.10f} {:16.10f} {:16.10f}'.format(*vec)
 
     def _removemol(self):
         s = self.settings.input
         system = s.find_case('system')
 
         if system in s:
-            if 'atoms' in s[system]:
-                del s[system]['atoms']
-            if 'lattice' in s[system]:
-                del s[system]['lattice']
+            for k in ['atoms', 'bonds', 'lattice']:
+                if k in s[system]:
+                    del s[system][k]
