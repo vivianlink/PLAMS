ca19b236d5cbf7fa95271c7f64e599cca1bc1caf
Michal Handzlik <handzlik@scm.com>
2017-08-15 14:21:00 +0200

    ADFNBOJob added to recipes

---
+++ src/scm/plams/recipes/adfnbo.py
@@ -0,0 +1,34 @@
+from ..interfaces.adfsuite.adf import ADFJob
+from ..core.common import log
+
+__all__ = ['ADFNBOJob']
+
+class ADFNBOJob(ADFJob):
+
+    def prerun(self):
+        s = self.settings.input
+        s[s.find_case('fullfock')] = True
+        s[s.find_case('aomat2file')] = True
+        s[s.find_case('symmetry')] = 'NOSYM'
+        save = s.find_case('save')
+        if save in s:
+            if isinstance(s.save, str):
+                s.save += ' TAPE15'
+            elif isinstance(s.save, list):
+                s.save.append('TAPE15')
+            else:
+                log("WARNING: 'SAVE TAPE15' could not be added to the input settings of {}. Make sure (thisjob).settings.input.save is a string or a list.".format(self.name), 1)
+        else:
+            s[save] = 'TAPE15'
+
+        self.results._rename_map['TAPE15'] = '$JN.t15'
+        self.settings.runscript.post =  """
+$ADFBIN/adfnbo <<eor
+write
+fock
+spherical
+end input
+eor
+
+$ADFBIN/gennbo6 FILE47
+"""
